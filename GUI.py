ddc3d908a8e245637d2fe103a0673202451e7e4a
1702609 <1702609@brunel.ac.uk>
2019-09-06 20:32:23 +0100

    Added hanging animation to the program

--- GUI.py
+++ GUI.py
@@ -1,29 +1,29 @@
-from tkinter import *
-
+import tkinter as tk
 import random
 from GameEngine import GameEngine
+from PIL import ImageTk, Image
 
 words = 'feminist functionalist globalisation verstehen postmodernism durkheim parson murdock murray becker ' \
             'interactionism simulacra phenomenology'.split()
 
-root = Tk()
-launchFrame = Frame(root, bg='#fcedcc', height=250, padx=5)
-
-
-top_frame = Frame(root, bg='cyan', width=500, height=50).pack()
+root = tk.Tk()
+launchFrame = tk.Frame(root, bg='#fcedcc', height=250, padx=5)
+top_frame = tk.Frame(root, bg='cyan', width=500, height=50).pack()
+#bottomFrame = tk.Frame(root, bg='#fcedcc', width=500, height=500).pack(side = tk.BOTTOM)
+canvas = tk.Canvas(root, width=500, height=500, bg = 'red')
 
 def gui_launcher():
-    root.wm_geometry("500x500")
-    introduction = Label(launchFrame, text='Please choose the following option:', bg='gold')
-    introduction.pack(side="top", fill=BOTH, expand=True)
-    start = Button(launchFrame, text='Start the game', fg='red')
+    root.wm_geometry("1000x600")
+    introduction = tk.Label(launchFrame, text='Please choose the following option:', bg='gold')
+    introduction.pack(side="top", fill=tk.BOTH, expand=True)
+    start = tk.Button(launchFrame, text='Start the game', fg='red')
     start.pack()
     start.bind('<Button-1>', startGame)
-    stats = Button(launchFrame, text='View stats (coming soon)', fg='blue')
+    stats = tk.Button(launchFrame, text='View stats (coming soon)', fg='blue')
     stats.pack()
-    exitB = Button(launchFrame, text='Exit', fg='green', bg='black')
+    exitB = tk.Button(launchFrame, text='Exit', fg='green', bg='black')
     exitB.pack()
-    launchFrame.pack(fill=X)
+    launchFrame.pack(fill=tk.X)
     root.mainloop()
 
 def getChoosenWord():
@@ -36,25 +36,33 @@ def startGame(event):
     for widget in launchFrame.winfo_children():
         widget.destroy()
     chosenWord = ge.getChoosenWord()
-    hiddenWord = Label(launchFrame, text=ge.getHiddenWord(), font=(None, 20))
+    global hiddenWord
+    hiddenWord = tk.Label(launchFrame, text=ge.getHiddenWord(), font=(None, 20))
     hiddenWord.place(x=25, y=25, anchor="center")
     hiddenWord.pack()
     print(chosenWord)
-    entryFrame=Frame(root, bg='#fcedcc')
-    Label(entryFrame, text="Guess a Letter: ").pack(side="left")
+    global entryFrame
+    entryFrame=tk.Frame(root, bg='#fcedcc')
+    tk.Label(entryFrame, text="Guess a Letter: ").pack(side="left")
     global inputU
-    inputU = Entry(entryFrame)
+    inputU = tk.Entry(entryFrame)
     inputU.pack(side="left")
-    submitButton = Button(entryFrame, text="Submit")
+    submitButton = tk.Button(entryFrame, text="Submit")
     submitButton.pack(side="left")
     submitButton.bind('<Button-1>', submitLetter)
     entryFrame.pack(pady=20)
     launchFrame.configure(bg='#fcedcc')
-    launchFrame.pack(fill=X)
+    launchFrame.pack(fill=tk.X)
 
 def submitLetter(event):
-    print(ge.isGuessCorrect(inputU.get()))
-
-
+    if ge.isGuessCorrect(inputU.get()) == True:
+        hiddenWord.config(text = ge.getUpdate(inputU.get()))
+    else:
+        picID = ge.getNumberOfFails()
+        img = ImageTk.PhotoImage(Image.open("img/pic"+str(picID)+".png"))
+        canvas.create_image(260, 400, image=img)
+        canvas.pack()
+        # The Label widget is a standard Tkinter widget used to display a text or image on the screen.
+        root.mainloop()
 
 gui_launcher()
--- GameEngine.py
+++ GameEngine.py
@@ -2,6 +2,8 @@
 class GameEngine:
     word = " "
     wordArray = []
+    dashArray = []
+    failNo = 0
 
     def __init__(self, choosenWord):
         self.word = choosenWord
@@ -19,7 +21,8 @@ class GameEngine:
         list_of_dash = []
         for dash1 in individual_list: #converts list to dashes
             list_of_dash.append(dash)
-        string_list_of_dash = ' '.join(list_of_dash)
+        self.dashArray = list_of_dash
+        string_list_of_dash = ' '.join(list_of_dash) #converts dashes array back to string
         dashes_together = string_list_of_dash
         return dashes_together
 
@@ -29,9 +32,20 @@ class GameEngine:
         for i in self.wordArray:
            if uInput == i:
                correctGuess = True
+        if correctGuess == False:
+            self.failNo +=1
         return correctGuess
 
+    def getUpdate(self, uInput):
+        index = 0
+        for i in self.wordArray:
+           if uInput == i:
+               self.dashArray[index] = i
+           index += 1
+        string_list_of_dash = ' '.join(self.dashArray)
+        return string_list_of_dash
 
-
+    def getNumberOfFails(self):
+        return self.failNo
 
 
